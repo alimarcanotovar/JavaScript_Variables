f666e859d19ee82f8557c158e59a0da83e63eddc
alimarcanotovar <aliymarcanotovar91@gmail.com>
2022-09-15 10:29:08 -0500

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,72 @@
-console.log("Hello World!\n==========\n");
+console.log("Hello World!");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
 // Exercise 1
+const firstName = "Ali";
+
+let lastName = "Marcano";
+
+var age = 31;
+
+console.log(`${firstName}`, `${lastName}`, `${age}`);
 
 // Exercise 2
+//firstName = "ali";
+
+lastName = "Marcano";
+
+age = 31;
+
+console.log(`${lastName}`, `${firstName}`, `${age}`);
 
 // Exercise 3
+let language = "JavaScript";
+
+let createdYear = 1991;
+
+let isCaseSensitive = true;
+
+console.log(`${language}`, `${createdYear}`, `${isCaseSensitive}`);
+
+//Exercise 4
+let price = 19.99;
+
+let isOnSale = false;
+
+let salePercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+console.log(
+  `${price}`,
+  `${isOnSale}`,
+  `${salePercentage}`,
+  `${stock}`,
+  `${inStock}`,
+  `${selectedSize}`
+);
+
+//Exercise 5
+let title = "Name of Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookmark = 456;
+
+let hasRead = true;
+
+console.log(
+  `${title}`,
+  `${author}`,
+  `${pageCount}`,
+  `${bookmark}`,
+  `${hasRead}`
+);
